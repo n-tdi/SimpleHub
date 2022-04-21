52227046e50220b6c48e63dfe5e52bd3d99a0f69
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-04-21 16:10:11 -0400

    init

---
+++ src/main/java/world/ntdi/simplehub/SimpleHub.java
@@ -0,0 +1,17 @@
+package world.ntdi.simplehub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleHub extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
