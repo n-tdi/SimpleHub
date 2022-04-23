20e8be4178bd279905d46ee4b2580226bbe15920
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-04-23 07:15:13 -0400

    Updated starter items to have glass panes

--- src/main/java/world/ntdi/simplehub/Util/StarterItems.java
+++ src/main/java/world/ntdi/simplehub/Util/StarterItems.java
@@ -9,15 +9,19 @@ import redempt.redlib.itemutils.ItemUtils;
 
 public class StarterItems {
     public static void setHotbar(Player p) {
+        for (int i = 0; i < 36; i++) {
+            p.getInventory().setItem(i, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName(""));
+        }
+
         ItemStack[] items = new ItemStack[9];
         items[0] = new ItemBuilder(Material.COMPASS).setName(TranslateColorCodes.translateColor("&8» &9&lNavigator"));
-        items[1] = new ItemBuilder(Material.FIREWORK_STAR).setName(TranslateColorCodes.translateColor("temp"));
-        items[2] = new ItemBuilder(Material.AIR);
-        items[3] = new ItemBuilder(Material.AIR);
-        items[4] = new ItemBuilder(Material.CHEST).setName(TranslateColorCodes.translateColor("temp"));
-        items[5] = new ItemBuilder(Material.AIR);
-        items[6] = new ItemBuilder(Material.AIR);
-        items[7] = new ItemBuilder(Material.NETHER_STAR).setName(TranslateColorCodes.translateColor("temp"));
+        items[1] = new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("");
+        items[2] = new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("");
+        items[3] = new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("");
+        items[4] = new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("");
+        items[5] = new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("");
+        items[6] = new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("");
+        items[7] = new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setName("");
         items[8] = new ItemBuilder(ItemUtils.skull(p)).setName(TranslateColorCodes.translateColor("&8» &9&l" + p.getName()));
 
         for (int i = 0; i < items.length; i++) {
