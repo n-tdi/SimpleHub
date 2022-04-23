acf22c8a8ce09334fd0e04669d821d6769fcc404
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-04-23 07:51:31 -0400

    Updated gamemode to adventure and join message

--- src/main/java/world/ntdi/simplehub/Listener/JoinListener.java
+++ src/main/java/world/ntdi/simplehub/Listener/JoinListener.java
@@ -1,5 +1,7 @@
 package world.ntdi.simplehub.Listener;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,6 +10,10 @@ import world.ntdi.simplehub.Util.StarterItems;
 public class JoinListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        StarterItems.setHotbar(e.getPlayer());
+        Player p = e.getPlayer();
+        StarterItems.setHotbar(p);
+        p.setGameMode(GameMode.ADVENTURE);
+        e.joinMessage(null);
+
     }
 }
