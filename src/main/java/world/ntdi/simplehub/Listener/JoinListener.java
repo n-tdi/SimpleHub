8675263fe6e1da57fef4d046d14dfeac4ca36acb
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-04-23 07:57:11 -0400

    updated adventure mode checks

--- src/main/java/world/ntdi/simplehub/Listener/JoinListener.java
+++ src/main/java/world/ntdi/simplehub/Listener/JoinListener.java
@@ -5,15 +5,23 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import world.ntdi.simplehub.SimpleHub;
 import world.ntdi.simplehub.Util.StarterItems;
 
 public class JoinListener implements Listener {
+    @SuppressWarnings("deprecation")
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         StarterItems.setHotbar(p);
-        p.setGameMode(GameMode.ADVENTURE);
-        e.joinMessage(null);
+        e.setJoinMessage(null);
 
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                p.setGameMode(GameMode.ADVENTURE);
+            }
+        }.runTaskLater(SimpleHub.getInstance(), 20L);
     }
 }
