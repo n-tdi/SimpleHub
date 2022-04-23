f927d626bc4196074b997cb7688f0135f4f35cfa
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-04-23 07:14:16 -0400

    Added checks for player dropping items

--- src/main/java/world/ntdi/simplehub/Listener/InventoryListener.java
+++ src/main/java/world/ntdi/simplehub/Listener/InventoryListener.java
@@ -3,10 +3,16 @@ package world.ntdi.simplehub.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 
 public class InventoryListener implements Listener {
     @EventHandler
     public void onInventoryEdit(InventoryClickEvent e) {
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent e) {
         e.setCancelled(true);
     }
 }
