18bb5c70afcdc4004cc938c2c1f15a469bf36282
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-04-23 07:51:14 -0400

    Disabled placing and brekaing blocks

---
+++ src/main/java/world/ntdi/simplehub/Listener/BlockListener.java
@@ -0,0 +1,23 @@
+package world.ntdi.simplehub.Listener;
+
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class BlockListener implements Listener {
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        if (e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        if (e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            e.setCancelled(true);
+        }
+    }
+}
